632808ef0fbb1ba64fe22ee90a5cd8d21ad9c70a
zvyaa <amandhacitra05@gmail.com>
2026-06-03 16:27:23 +0800

    Modul 6 - Pengenalan CodeIgniter

---
+++ PRAK601/app/Controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\ProfilModel;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        $model = new ProfilModel();
+        $data = $model->getProfil();
+        return view('beranda', $data);
+    }
+
+    public function profil()
+    {
+        $model = new ProfilModel();
+        $data = $model->getProfil();
+        return view('profil', $data);
+    }
+}
